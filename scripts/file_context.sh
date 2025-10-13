1f3560cf2555205fed315b045ee77f80cac98af1
Majaahh <maja.blaszczak@proton.me>
2025-10-13 22:08:22 +0200

    samsung: Drop firmware blobs in file_context/fs_config
    Change-Id: I2b5f48769f6df05316ae5428f4d7abdecb3ab263
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- scripts/file_context.sh
+++ scripts/file_context.sh
@@ -29,18 +29,7 @@ generate_file_contexts() {
 
 [[ ! -d "file_context/$BOARD" ]] && mkdir -p "file_context/$BOARD"
 
-{
-    if ! sudo grep -q "m34" "vendor/mount/build.prop"; then
-        echo "/vendor/firmware/AP_AUDIO_SLSI\.bin u:object_r:vendor_fw_file:s0"
-        echo "/vendor/firmware/APDV_AUDIO_SLSI\.bin u:object_r:vendor_fw_file:s0"
-    fi
-    echo "/vendor/firmware/calliope_sram\.bin u:object_r:vendor_fw_file:s0"
-    echo "/vendor/firmware/mfc_fw\.bin u:object_r:vendor_fw_file:s0"
-    echo "/vendor/firmware/NPU\.bin u:object_r:vendor_npu_firmware_file:s0"
-    echo "/vendor/firmware/os\.checked\.bin u:object_r:vendor_fw_file:s0"
-    echo "/vendor/firmware/vts\.bin u:object_r:vendor_fw_file:s0"
-    generate_file_contexts "vendor/tee" "u:object_r:tee_file:s0"
-} > "file_context/$BOARD/file.${MODEL}_${CSC}_${OMC}"
+generate_file_contexts "vendor/tee" "u:object_r:tee_file:s0" > "file_context/$BOARD/file.${MODEL}_${CSC}_${OMC}"
 
 mv vendor/tee vendor/tee_old
 mkdir -p vendor/tee/${MODEL}
@@ -49,16 +38,6 @@ cp -rfa vendor/tee_old/* vendor/tee/${MODEL}
 {
     echo ""
     echo "# Custom Path"
-    echo "/vendor/firmware/${MODEL} u:object_r:vendor_fw_file:s0"
-    if ! sudo grep -q "m34" "vendor/mount/build.prop"; then
-        echo "/vendor/firmware/${MODEL}/AP_AUDIO_SLSI\.bin u:object_r:vendor_fw_file:s0"
-        echo "/vendor/firmware/${MODEL}/APDV_AUDIO_SLSI\.bin u:object_r:vendor_fw_file:s0"
-    fi
-    echo "/vendor/firmware/${MODEL}/calliope_sram\.bin u:object_r:vendor_fw_file:s0"
-    echo "/vendor/firmware/${MODEL}/mfc_fw\.bin u:object_r:vendor_fw_file:s0"
-    echo "/vendor/firmware/${MODEL}/NPU\.bin u:object_r:vendor_npu_firmware_file:s0"
-    echo "/vendor/firmware/${MODEL}/os\.checked\.bin u:object_r:vendor_fw_file:s0"
-    echo "/vendor/firmware/${MODEL}/vts\.bin u:object_r:vendor_fw_file:s0"
     generate_file_contexts "vendor/tee/${MODEL}" "u:object_r:tee_file:s0"
 } >> "file_context/$BOARD/file.${MODEL}_${CSC}_${OMC}"
 
--- scripts/fs_config.sh
+++ scripts/fs_config.sh
@@ -26,12 +26,7 @@ BLOBS=( "calliope_sram.bin" "mfc_fw.bin" "NPU.bin" "os.checked.bin" "vts.bin" )
 ! sudo grep -q "m34" "vendor/mount/build.prop" && BLOBS+=("AP_AUDIO_SLSI.bin" "APDV_AUDIO_SLSI.bin")
 [[ ! -d "fs_config/$BOARD" ]] && mkdir -p "fs_config/$BOARD"
 
-{
-    for i in "${BLOBS}"; do
-        [[ -f "vendor/firmware/$i" ]] && echo "vendor/firmware/$i 0 0 644 capabilities=0x0"
-    done
-    generate_entries "vendor/tee"
-} > "fs_config/$BOARD/fs.${MODEL}_${CSC}_${OMC}"
+generate_entries "vendor/tee" > "fs_config/$BOARD/fs.${MODEL}_${CSC}_${OMC}"
 
 mkdir -p vendor/tee/${MODEL}
 cp -rfa vendor/tee_old/* vendor/tee/${MODEL}
@@ -40,8 +35,5 @@ cp -rfa vendor/tee_old/* vendor/tee/${MODEL}
     echo ""
     echo "# Custom Path"
     echo "vendor/firmware/${MODEL} 0 0 644 capabilities=0x0"
-    for i in "${BLOBS}"; do
-        [[ -f "vendor/firmware/$i" ]] && echo "vendor/firmware/$MODEL/$i 0 0 644 capabilities=0x0"
-    done
     generate_entries "vendor/tee/${MODEL}"
 } >> "fs_config/$BOARD/fs.${MODEL}_${CSC}_${OMC}"
